6526a3627d4c4ebc53a4e9311d17821faa95375c
mdurgeshaxeno <146161081+mdurgeshaxeno@users.noreply.github.com>
2023-10-16 12:44:18 +0530

    Update card-carousel.js

--- blocks/card-carousel/card-carousel.js
+++ blocks/card-carousel/card-carousel.js
@@ -3,7 +3,7 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
-  ul.classList.add('owl-carousel', 'owl-theme', 'card-carousel');
+  ul.classList.add('owl-carousel', 'owl-theme', 'card-carousel-list');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
