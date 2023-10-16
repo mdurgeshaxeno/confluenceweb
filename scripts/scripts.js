cdbcf0f14bbdf365bff4d3d08f908277e8d0005a
mdurgeshaxeno <146161081+mdurgeshaxeno@users.noreply.github.com>
2023-10-16 12:46:58 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,3 @@
-$(document).ready(function() {
-  var owl = $('.card-carousel-list');
-  owl.owlCarousel({
-    items: 4,
-    loop: true,
-    margin: 10,
-    dots: false,
-    nav: true,
-    autoplay: true,
-    autoplayTimeout: 1000,
-    autoplayHoverPause: true,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 2
-      },
-      1000: {
-        items: 3
-      }
-    }
-  });
-});
-
 import {
   sampleRUM,
   buildBlock,
@@ -446,3 +420,30 @@ async function loadPage() {
 }
 
 loadPage();
+
+
+$(document).ready(function() {
+  var owl = $('.card-carousel-list');
+  owl.owlCarousel({
+    items: 4,
+    loop: true,
+    margin: 10,
+    dots: false,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+});
