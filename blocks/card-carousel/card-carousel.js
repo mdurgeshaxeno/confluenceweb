9539ccdcf74f23e1d7bcf49d539b9c9c6b2636ee
mdurgeshaxeno <146161081+mdurgeshaxeno@users.noreply.github.com>
2023-10-16 10:47:44 +0530

    Update card-carousel.js
    removed Script

--- blocks/card-carousel/card-carousel.js
+++ blocks/card-carousel/card-carousel.js
@@ -17,30 +17,3 @@ export default function decorate(block) {
   block.textContent = '';
   block.append(ul);
 }
-
-
-$(document).ready(function() {
-  var owl = $('.card-carousel');
-  owl.owlCarousel({
-    items: 4,
-    loop: true,
-    margin: 10,
-    dots:false,
-    nav:true,
-    autoplay: true,
-    autoplayTimeout: 1000,
-    autoplayHoverPause: true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
-    }
-  });
-})
