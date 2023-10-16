dc74453676db3190f67cbcf8f21472babb1b734b
mdurgeshaxeno <146161081+mdurgeshaxeno@users.noreply.github.com>
2023-10-16 12:02:46 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -420,29 +420,3 @@ async function loadPage() {
 }
 
 loadPage();
-
-$(document).ready(function() {
-  var owl = $('.card-carousel');
-  owl.owlCarousel({
-    items: 4,
-    loop: true,
-    margin: 10,
-    dots: false,
-    nav: true,
-    autoplay: true,
-    autoplayTimeout: 1000,
-    autoplayHoverPause: true,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 2
-      },
-      1000: {
-        items: 3
-      }
-    }
-  });
-});
